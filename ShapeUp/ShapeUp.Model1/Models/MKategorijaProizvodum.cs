f7bfaa70eab921527d5b10a8a4feb5a4cb3764eb
Miralem Omerika <41287215+neiraomerika@users.noreply.github.com>
2021-09-08 20:01:52 +0200

    added shapeup.model

---
+++ ShapeUp/ShapeUp.Model1/Models/MKategorijaProizvodum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeUp.Model.Models
+{
+   public  class MKategorijaProizvodum
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+    }
+}
