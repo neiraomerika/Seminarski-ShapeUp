76a288f402480581826b5e29b9735d2c94cfd40f
Miralem Omerika <miralem.omerika@edu.fit.ba>
2022-01-12 23:45:49 +0100

    started users form

--- ShapeUp/ShapeUp.Desktop/Training/frmShowTrainings.Designer.cs
+++ ShapeUp/ShapeUp.Desktop/Training/frmShowTrainings.Designer.cs
@@ -30,18 +30,18 @@ namespace ShapeUp.Desktop.Training
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvTrainings = new System.Windows.Forms.DataGridView();
-            this.cmbTCateg = new System.Windows.Forms.ComboBox();
-            this.btnSearch = new System.Windows.Forms.Button();
-            this.btnClearSearch = new System.Windows.Forms.Button();
-            this.mTreningBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Slika = new System.Windows.Forms.DataGridViewImageColumn();
             this.opisDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.videoUrlDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ciljIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.kategorijaTreningaIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmbTCateg = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClearSearch = new System.Windows.Forms.Button();
+            this.mTreningBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dgvTrainings)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mTreningBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -66,44 +66,12 @@ namespace ShapeUp.Desktop.Training
             this.dgvTrainings.TabIndex = 0;
             this.dgvTrainings.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dgvTrainings_MouseDoubleClick);
             // 
-            // cmbTCateg
-            // 
-            this.cmbTCateg.FormattingEnabled = true;
-            this.cmbTCateg.Location = new System.Drawing.Point(13, 10);
-            this.cmbTCateg.Name = "cmbTCateg";
-            this.cmbTCateg.Size = new System.Drawing.Size(180, 21);
-            this.cmbTCateg.TabIndex = 1;
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.Location = new System.Drawing.Point(199, 9);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(135, 23);
-            this.btnSearch.TabIndex = 3;
-            this.btnSearch.Text = "Pretrazi";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
-            // 
-            // btnClearSearch
-            // 
-            this.btnClearSearch.Location = new System.Drawing.Point(340, 9);
-            this.btnClearSearch.Name = "btnClearSearch";
-            this.btnClearSearch.Size = new System.Drawing.Size(135, 23);
-            this.btnClearSearch.TabIndex = 4;
-            this.btnClearSearch.Text = "Ocisti Filtere";
-            this.btnClearSearch.UseVisualStyleBackColor = true;
-            this.btnClearSearch.Click += new System.EventHandler(this.btnClearSearch_Click);
-            // 
-            // mTreningBindingSource
-            // 
-            this.mTreningBindingSource.DataSource = typeof(ShapeUp.Model.Models.MTrening);
-            // 
             // idDataGridViewTextBoxColumn
             // 
             this.idDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
-            dataGridViewCellStyle2.NullValue = "byte[0]";
-            this.idDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle1.NullValue = "byte[0]";
+            this.idDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle1;
             this.idDataGridViewTextBoxColumn.HeaderText = "Id";
             this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
             this.idDataGridViewTextBoxColumn.ReadOnly = true;
@@ -144,6 +112,39 @@ namespace ShapeUp.Desktop.Training
             this.kategorijaTreningaIdDataGridViewTextBoxColumn.HeaderText = "Kategorija Treninga";
             this.kategorijaTreningaIdDataGridViewTextBoxColumn.Name = "kategorijaTreningaIdDataGridViewTextBoxColumn";
             this.kategorijaTreningaIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // cmbTCateg
+            // 
+            this.cmbTCateg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cmbTCateg.FormattingEnabled = true;
+            this.cmbTCateg.Location = new System.Drawing.Point(13, 10);
+            this.cmbTCateg.Name = "cmbTCateg";
+            this.cmbTCateg.Size = new System.Drawing.Size(180, 28);
+            this.cmbTCateg.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(199, 9);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(135, 29);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Pretrazi";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClearSearch
+            // 
+            this.btnClearSearch.Location = new System.Drawing.Point(340, 9);
+            this.btnClearSearch.Name = "btnClearSearch";
+            this.btnClearSearch.Size = new System.Drawing.Size(135, 29);
+            this.btnClearSearch.TabIndex = 4;
+            this.btnClearSearch.Text = "Ocisti Filtere";
+            this.btnClearSearch.UseVisualStyleBackColor = true;
+            this.btnClearSearch.Click += new System.EventHandler(this.btnClearSearch_Click);
+            // 
+            // mTreningBindingSource
+            // 
+            this.mTreningBindingSource.DataSource = typeof(ShapeUp.Model.Models.MTrening);
             // 
             // frmShowTrainings
             // 
---
+++ ShapeUp/ShapeUp.Desktop/Users/frmShowUsers.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace ShapeUp.Desktop.Users
+{
+    partial class frmShowUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.cmbActive = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToAddRows = false;
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Location = new System.Drawing.Point(13, 100);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUsers.Size = new System.Drawing.Size(775, 338);
+            this.dgvUsers.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtSearch.Location = new System.Drawing.Point(13, 13);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(232, 23);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // cmbActive
+            // 
+            this.cmbActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cmbActive.FormattingEnabled = true;
+            this.cmbActive.Location = new System.Drawing.Point(274, 12);
+            this.cmbActive.Name = "cmbActive";
+            this.cmbActive.Size = new System.Drawing.Size(132, 24);
+            this.cmbActive.TabIndex = 2;
+            // 
+            // frmShowUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbActive);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.dgvUsers);
+            this.Name = "frmShowUsers";
+            this.Text = "frmShowUsers";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ComboBox cmbActive;
+    }
+}
