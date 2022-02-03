5ce9807d2713960ede5ef0da0e86997c3c429056
Neira Omerika <neira.omerika@edu.fit.ba>
2022-02-04 00:12:42 +0100

    created payment controller

---
+++ ShapeUp/ShapeUp/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShapeUp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : Controller
+    {
+        private Token stripeToken;
+        private TokenService tokenService;
+
+        private string stripePublishableApiKey = "pk_test_51KPCVJGaEg6pakfZs5knHOXZKybwOTLc9xfoHwT0BNZFJe8ccyAPvv6VejtT79GAvs7OqkdbjDmpeH3mweI7FP7Y00nzh2tpJx";
+        private string stripeSecretApiKey = "sk_test_51KPCVJGaEg6pakfZMXk3ctLRAl6jcGKFUyeNkDEKW5Agw9Rtjg9zs8l05bJ9NWP394pHf4zoJTE7CJuMhMP1Tk5200NlTRNaM8";
+    }
+}
